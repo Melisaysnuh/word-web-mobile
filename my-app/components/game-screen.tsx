2cbefaf1354acdd563eec8f78dfcafed85cb32c9
Melisaysnuh <melissacessna@yahoo.com>
2024-11-19 20:37:58 +0100

    started laying out basic conversion

--- my-app/components/game-screen.tsx
+++ my-app/components/game-screen.tsx
@@ -4,7 +4,16 @@ import { StyleSheet, Text, View } from 'react-native';
 const GameScreen: React.FC = () => {
     return (
         <View style={gameScreenStyles.container}>
-            <Text style={gameScreenStyles.mainText}>Game Screen here!</Text>
+            <View style={gameScreenStyles.nav} id='progress'>
+                <Text style={gameScreenStyles.mainText}>Nav Bar</Text></View>
+            <View style={gameScreenStyles.progress} id='progress'>
+                <Text style={gameScreenStyles.mainText}>Progress Bar</Text></View>
+            <View style={gameScreenStyles.output} id='output'>
+                <Text style={gameScreenStyles.mainText}>Output</Text></View>
+            <View style={gameScreenStyles.gameholder} id='gameholder'>
+                <Text style={gameScreenStyles.mainText}>Game Holder </Text></View>
+            <View style={gameScreenStyles.controls} id='controls'>
+                <Text style={gameScreenStyles.mainText}>Controls</Text></View>
 
         </View>
     );
@@ -15,16 +24,48 @@ export interface GameScreenProps {
 }
 const gameScreenStyles = StyleSheet.create({
     container: {
-        flex: 1,
         backgroundColor: '#13072E',
         alignItems: 'center',
         justifyContent: 'center',
+        width: '100%',
+        height: '100%'
     },
+    progress: {
+        width: '100%',
+        height: '15%',
+        backgroundColor: 'blue'
+    },
+    nav: {
+        width: '100%',
+        height: '10%',
+        backgroundColor: 'red'
+    },
+    output: {
+        borderCurve: 'circular',
+        borderColor: 'white',
+        width: '100%',
+        height: '10%',
+        backgroundColor: 'blue'
+    },
+    gameholder: {
+        borderCurve: 'circular',
+        borderColor: 'white',
+        width: '100%',
+        height: '40%',
+        backgroundColor: 'blue'
+    },
+    controls: {
+        borderCurve: 'circular',
+        borderColor: 'white',
+        width: '100%',
+        height: '20%',
+        backgroundColor: 'blue'
+    },
+
+
     mainText: {
-        flex: 1,
         color: 'white',
-        alignItems: 'center',
-        justifyContent: 'center',
+
     },
 });
 export default GameScreen;
